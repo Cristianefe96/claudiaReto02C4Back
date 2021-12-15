a5249e79cfc52fa7fa9a2a3d245869f00aaba84b
CRISTIAN FABIAN GOMEZ <cristianf.gomez.mt@correo.usa.edu.co>
2021-12-15 15:16:05 -0500

    cambios en el controller

--- src/main/java/retosciclo4/reto02/controller/UserController.java
+++ src/main/java/retosciclo4/reto02/controller/UserController.java
@@ -1,6 +1,8 @@
 package retosciclo4.reto02.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -29,10 +31,10 @@ public class UserController {
         return userService.getAll();
     }
     
-    // @GetMapping("/{id}")
-    // public Optional <User> getUser(@PathVariable("id") int id) {
-    //     return userService.getUser(id);
-    // }
+    @GetMapping("/{id}")
+    public Optional <User> getUser(@PathVariable("id") int id) {
+        return userService.getUser(id);
+    }
 
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
